d5181343a030ea8ba49154df2cff24286aea82e6
SajedehGharabdian <sajedehgharabadian78@gmail.com>
2024-05-27 21:00:43 +0330

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+this package for sodoku detection
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def preinstall():
+    f = open("readme.md","r")
+    text = f.read()
+    return text
+
+
+setup(name="sodoku_detection",version="1.0.0",
+      author="Sajedeh Gharabadiyan",
+      description="first package for test",
+      requires=[],
+      author_email="gharabadiyansajedeh@gmail.com",
+      packages=["sodoku_detection"],
+      long_description=open("readme.md", 'r').read(),
+      long_description_content_type='text/markdown'
+    )
